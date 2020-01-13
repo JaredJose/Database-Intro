9a6b59dbf4331934c64ddbe31f762a4b030a6a85
JaredJose <42820125+JaredJose@users.noreply.github.com>
2020-01-13 13:02:06 -0800

    Implemented commented code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,114 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-
+	public static void main(String[] args) {
+		createNewTable(); //Creating the table within the database file
+		displayAll(); //Displaying all the data within the table in a formatted fashion
+		System.out.println(); //Break line between table outputs
+		addStudent("Jeremiah Johnson", 16, 11, "Del Norte"); //Adds a student to the database - modify the values to add yourself!
+		displayAll(); 
+		System.out.println();
+		deleteStudent("Jeremiah Johnson"); // Deleting the student from the database based off of their name
+		displayAll();
+		System.out.println();
+	}
+	
+	//Creates a table within the database if it doesn't already exist
+	public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:Database.db"; //Setting up url which leads to the database file
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS students (\n"
+        		+ "name text NOT NULL, \n"
+        		+ "age integer NOT NULL, \n"
+        		+ "grade integer NOT NULL, \n"
+        		+ "school text NOT NULL, \n"
+        		+ "id integer PRIMARY KEY AUTOINCREMENT\n"
+        		+ ");";
+        
+        //Attempts to connect to the file you listed in the url, then creates a statement to prepare to execute the sql string
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	//Connects to the database
+	public static Connection connect() {
+		String url = "jdbc:sqlite:Database.db"; //url tells the driver manager where to locate the Database.db file
+		Connection conn = null;
+		
+		//Attempts to connect to the database file
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn; //returns a connection object
+	}
+	
+	//Displays all of the information that is stored in the table 
+	public static void displayAll(){
+		//Selecting the columns you'd like to get information from
+        String sql = "SELECT Name, Age, Grade, School FROM students";
+        
+        //Connecting to the database then preparing the objects to receive the info
+        try (Connection conn = connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set, outputting the information row by row
+            while (rs.next()) {
+                System.out.println(rs.getString("Name") +  "\t" + 
+                                   rs.getInt("Age") + "\t" +
+                                   rs.getInt("Grade") + "\t" +
+                                   rs.getString("School"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	//Add a student to the students table within the database
+	public static void addStudent(String name, int age, int grade, String school) {
+		try {
+			Connection conn = connect();
+			//specifying the column names allows you to insert data into not all the columns through the prepared statement
+			PreparedStatement prep = conn.prepareStatement("INSERT INTO students (name, age, grade, school) values(?, ?, ?, ?);");
+			prep.setString(1, name); //load the name into the statement
+			prep.setInt(2, age); //load the age into the statement
+			prep.setInt(3, grade); //load the grade into the statement
+			prep.setString(4, school); //load the school name into the statement
+			prep.execute(); //write the information to the table
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Delete a student from the students table within the database file
+	public static void deleteStudent(String name) {
+        String sql = "DELETE FROM students WHERE name = ?"; //Deleting a student from the table based off of their name
+ 
+        //try to connect to the database
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+ 
+            // set the corresponding param
+            pstmt.setString(1, name);
+            // execute the delete statement
+            pstmt.executeUpdate();
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
